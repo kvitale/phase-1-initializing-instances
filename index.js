c122e2c63514f9aad37fbd470bcbd09b581e8541
Kyle <ktvitale7@gmail.com>
2022-06-09 12:16:39 -0400

    commit tree d27214d61b457c2a6b76df8d4e28d7f738ba9a43

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink){
+    this.food = food
+    this.drink = drink
+    
+    }
+    
+}
+let bfast = new Breakfast("eggs", "juice")
+bfast.food 
+bfast.drink
+
+
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+const newlunch = new Lunch("side salad", "broccoli cheddar soup", "iced tea")
+newlunch.salad
+newlunch.soup
+newlunch.drink
+
+
+class Dinner {
+    #dessert
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this.#dessert = dessert
+        
+    }
+}
+const newDinner = new Dinner("balsamic salad", "consomme","filet mignon")
+newDinner.salad
+newDinner.soup
+newDinner.entree
